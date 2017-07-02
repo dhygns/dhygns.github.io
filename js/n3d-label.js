19827979c1fa7af5c6314463d1a9277e5ded5e43
donghyeon kim <dhygns@naver.com>
2017-07-02 12:55:39 +0900

    style sheet updated & fixed bug

---
+++ js/n3d-label.js
@@ -0,0 +1,63 @@
+
+class Div_Label extends Main {
+  constructor(mainid) {
+    super("div_label");
+
+
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.Camera();
+
+    this.uniforms = {
+      unif_mouse : { type : "2f", value : [0.0, 0.0] },
+      unif_time : { type : "1f", value : 0.0 }
+    };
+
+    this.object = new THREE.Object3D();
+    this.object.add(new THREE.Mesh(
+      new THREE.PlaneGeometry(2.0, 2.0),
+      new THREE.ShaderMaterial({
+        uniforms : this.uniforms,
+        fragmentShader : `
+        #define PI ` + Math.PI + `
+
+        uniform vec2 unif_mouse;
+        uniform float unif_time;
+
+        varying vec2 vtex;
+
+        void main(void) {
+          vec3 color = vec3(0.0);
+          float pivr = (unif_time * 1.0 + vtex.x * 3.0 + unif_mouse.x * 10.0) * PI;
+          float pivg = (unif_time * 0.2 + vtex.x * 2.0 + unif_mouse.x * 3.0) * PI;
+          color.r = 0.5 + 0.5 * smoothstep(0.5 + 0.3 * sin(pivr), 0.5 + 0.3 * sin(pivr) - 0.2, vtex.y);
+          color.g = 0.5 + 0.5 * smoothstep(0.5 + 0.3 * sin(pivg), 0.5 + 0.3 * sin(pivg) - 0.2, vtex.y);
+          color.b = 0.2 + 0.8 * smoothstep(1.0, 0.5, vtex.y);
+
+          gl_FragColor = vec4(color, 1.0);
+        }
+        `,
+        vertexShader : `
+        varying vec2 vtex;
+        void main(void) {
+          vtex = uv;
+          gl_Position = vec4(position, 1.0);
+        }
+        `
+      })
+    ));
+    this.scene.add(this.object);
+  }
+
+  onMousemove(x, y) {
+    this.uniforms.unif_mouse.value[0] = x;
+    this.uniforms.unif_mouse.value[1] = y;
+  }
+
+  update(rdrr, dt) {
+    super.update();
+
+    this.uniforms.unif_time.value += dt;
+
+    rdrr.render(this.scene, this.camera);
+  }
+}
--- js/n3d-main.js
+++ js/n3d-main.js
@@ -4,7 +4,7 @@
 class Main {
   constructor(mainid) {
     this.rdrr = new THREE.WebGLRenderer({alpha : false});
-    this.dom = document.getElementById("main_top");
+    this.dom = document.getElementById(mainid);
 
     if(this.dom == null) {
       console.warn(mainid + " doesn't exist.");
@@ -17,8 +17,10 @@ class Main {
 
     //regist event listeners
     window.addEventListener("resize", (e) => {
+      console.log(this.dom.offsetHeight);
       this.rdrr.setSize(this.dom.offsetWidth, this.dom.offsetHeight);
     });
+
     this.dom.addEventListener("mousemove", ({pageX, pageY})=>{
       const x = 0.0 + pageX / this.dom.offsetWidth;
       const y = 1.0 - pageY / this.dom.offsetHeight;
@@ -39,67 +41,5 @@ class Main {
     this.newt = new Date();
 
     requestAnimationFrame(this.update.bind(this, this.rdrr, (this.newt - this.oldt) * 0.001));
-  }
-}
-
-
-class Top_Main extends Main {
-  constructor(mainid) {
-    super("main_top");
-
-
-    this.scene = new THREE.Scene();
-    this.camera = new THREE.Camera();
-
-    this.uniforms = { unif_mouse : { type : "2f", value : [0.0, 0.0] } };
-
-    this.object = new THREE.Object3D();
-    this.object.add(new THREE.Mesh(
-      new THREE.PlaneGeometry(2.0, 2.0),
-      new THREE.ShaderMaterial({
-        uniforms : this.uniforms,
-        fragmentShader : `
-        #define PI ` + Math.PI + `
-        varying vec2 vmouse;
-        varying vec2 vtex;
-
-        void main(void) {
-          vec3 color = vec3(0.0);
-          color.r = 0.5 + 0.5 * abs(sin((vtex.x + vmouse.x) * PI));
-          color.g = 0.5 + 0.5 * abs(cos((vtex.x + vmouse.y) * PI));
-          color.b = 0.5 + 0.5 * abs(sin((vtex.x + 1.2 * vmouse.y) * PI));
-
-          gl_FragColor = vec4(color, 1.0);
-        }
-        `,
-        vertexShader : `
-        uniform vec2 unif_mouse;
-        varying vec2 vmouse;
-        varying vec2 vtex;
-        void main(void) {
-          vmouse = unif_mouse;
-          vtex = uv;
-          gl_Position = vec4(position, 1.0);
-        }
-        `
-      })
-    ));
-    this.scene.add(this.object);
-  }
-
-  onMousemove(x, y) {
-    this.uniforms.unif_mouse.value[0] = x;
-    this.uniforms.unif_mouse.value[1] = y;
-  }
-
-  update(rdrr, dt) {
-    super.update();
-    rdrr.render(this.scene, this.camera);
-  }
-}
-
-class Left_Main extends Main {
-  constructor(mainid) {
-    super("main_left");
   }
 }
